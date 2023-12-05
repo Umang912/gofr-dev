35af6af774940ae44d07dec031e27a31a800fa70
Umang Sharma <81818376+Umang912@users.noreply.github.com>
2023-12-05 23:40:03 +0530

    Add files via upload
    Vehicle Table stores the vehicle details including make, model, and entry/exit times in the parking area.
    Parking Table manages the parking locations, recording names, locations, and maximum capacities.
    Maintenance Table tracks vehicle maintenance activities, recording types, durations, and completion status.

---
+++ SQLQuery4.sql
@@ -0,0 +1,30 @@
+create database data;
+
+CREATE TABLE Vehicle (
+    VehicleID INT PRIMARY KEY IDENTITY(1,1),
+    Make VARCHAR(255),
+    Model VARCHAR(255),
+    LicensePlate VARCHAR(50),
+    EntryTime DATETIME,
+    ExitTime DATETIME,
+    IsParked BIT
+);
+
+
+CREATE TABLE Parking (
+    ParkingID INT PRIMARY KEY IDENTITY(1,1),
+    Name VARCHAR(100),
+    Location VARCHAR(255),
+    Capacity INT
+);
+
+
+CREATE TABLE Maintenance (
+    MaintenanceID INT PRIMARY KEY IDENTITY(1,1),
+    VehicleID INT,
+    MaintenanceType VARCHAR(100),
+    StartTime DATETIME,
+    EndTime DATETIME,
+    IsCompleted BIT,
+    FOREIGN KEY (VehicleID) REFERENCES Vehicle(VehicleID)
+);
