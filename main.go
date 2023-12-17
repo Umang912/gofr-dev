8ff91974ad6c6886640f0dc8eec6215712d9168b
Umang Sharma <81818376+Umang912@users.noreply.github.com>
2023-12-17 21:50:29 +0530

    main.go

--- main.go
+++ main.go
@@ -1,48 +1,24 @@
 package main
 
 import (
-	"database/sql"
-	"encoding/json"
-	"log"
-	"net/http"
-	"time"
+	"GO-LANG/datastore"
+	"GO-LANG/handler"
 
-	"github.com/gorilla/mux"
-	_ "github.com/go-sql-driver/mysql"
+	"gofr.dev/pkg/gofr"
 )
 
-var db *sql.DB
-
-type Vehicle struct {
-	VehicleID    int       `json:"VehicleID"`
-	Make         string    `json:"Make"`
-	Model        string    `json:"Model"`
-	LicensePlate string    `json:"LicensePlate"`
-	EntryTime    time.Time `json:"EntryTime"`
-	ExitTime     time.Time `json:"ExitTime"`
-	IsParked     bool      `json:"IsParked"`
-}
-
-type Parking struct {
-	ParkingID int    `json:"ParkingID"`
-	Name      string `json:"Name"`
-	Location  string `json:"Location"`
-	Capacity  int    `json:"Capacity"`
-}
-
-type Maintenance struct {
-	MaintenanceID   int       `json:"MaintenanceID"`
-	VehicleID       int       `json:"VehicleID"`
-	MaintenanceType string    `json:"MaintenanceType"`
-	StartTime       time.Time `json:"StartTime"`
-	EndTime         time.Time `json:"EndTime"`
-	IsCompleted     bool      `json:"IsCompleted"`
-}
-
 func main() {
-	var err error
-	db, err = sql.Open("mysql", "username:password@tcp(your-host:your-port)/your-database")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
+	app := gofr.New()
+
+	// Assuming `datastore.New()` initializes an Inventory datastore instance
+	s := datastore.NewInventory()
+	h := handler.New(*s)
+
+	app.GET("/product/{ID}", h.GetProductByID)
+	app.POST("/product", h.AddProduct)
+	app.DELETE("/product/{ID}", h.RemoveProduct)
+	app.PUT("/product/{ID}", h.UpdateProduct)
+
+	app.Server.HTTP.Port = 9092
+	app.Start()
+}
